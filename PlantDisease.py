06652927ca45f0619ddb293417d0f91e4a090279
Howard Huang <howardhuang2004@gmail.com>
2021-06-27 01:48:47 -0700

    upload plant disease dataset

---
+++ PlantDisease.py
@@ -0,0 +1,31 @@
+import os
+import numpy as np
+import pandas as pd
+import torch
+import matplotlib.pyplot as plt
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from PIL import Image
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torchvision.utils import make_grid
+from torchvision.datasets import ImageFolder
+from torchsummary import summary
+
+data_dir = "/Users/howardhuang/Documents/Harvest/new-plant-diseases-dataset/New Plant Diseases Dataset(Augmented)"
+train_dir = data_dir + "/train"
+valid_dir = data_dir + "/valid"
+diseases = os.listdir(train_dir)
+
+# print all disease names 
+print(diseases)
+
+print("Total disease classes are: {}".format(len(diseases)))
+
+plants = []
+NumberOfDiseases = 0
+for plant in diseases:
+    if plant.split('___')[0] not in plants:
+        plants.append(plant.split('___'[0]))
+    if plant.split('___')[1] != 'healthy':
+        NumberOfDiseases += 1
