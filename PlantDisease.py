33956eda663559e9e32716ddda61756f6f50d3c2
Howard Huang <howardhuang2004@gmail.com>
2021-06-27 11:41:15 -0700

    Update PlantDisease.py

--- PlantDisease.py
+++ PlantDisease.py
@@ -52,3 +52,66 @@ valid = ImageFolder(valid_dir, transform=transforms.ToTensor())
 
 img, label = train[0]
 print(img.shape, label)
+
+# Setting the seed value
+random_seed = 7
+torch.manual_seed(random_seed)
+
+# setting the batch size
+batch_size = 32
+
+# DataLoaders for training and validation
+train_dl = DataLoader(train, batch_size, shuffle=True, num_workers=2, pin_memory=True)
+valid_dl = DataLoader(valid, batch_size, num_workers=2, pin_memory=True)
+
+# for moving data into GPU (if available)
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available:
+        return torch.device("cuda")
+    else:
+        return torch.device("cpu")
+
+# for moving data to device (CPU or GPU)
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list,tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+# for loading in the device (GPU if available else CPU)
+class DeviceDataLoader():
+    """Wrap a dataloader to move data to a device"""
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+        
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl:
+            yield to_device(b, self.device)
+        
+    def __len__(self):
+        """Number of batches"""
+        return len(self.dl)
+
+device = get_default_device()
+device
+
+# Moving data into GPU
+train_dl = DeviceDataLoader(train_dl, device)
+valid_dl = DeviceDataLoader(valid_dl, device)
+
+class SimpleResidualBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        return self.relu2(out) + x # ReLU can be applied before or after adding the input
